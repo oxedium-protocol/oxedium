0e1f058c4dd2509b99391599019172361b207c01
Arj-Lab <arj.lemited@gmail.com>
2025-12-02 17:59:56 +0300

    update: links

--- app/lib/utils/links.dart
+++ app/lib/utils/links.dart
@@ -1,6 +1,6 @@
 const String whitepaperLink = "https://4dac7oaqhkztsapbsfnjdxiau4yhvykfizpgwajvw2h7xr2bz3qq.arweave.net/4MAvuBA6szkB4ZFakd0ApzB64UVGXmsBNbaP-8dBzuE";
 
-const String twitterLink = "https://x.com/tyrbine";
+const String twitterLink = "https://x.com/0xedium";
 const String telegramLink = "https://t.me/oxedium_protocol";
 const String telegramChatLink = "https://t.me/oxedium_portal";
 const String repGithubLink = "https://github.com/oxedium-protocol";
