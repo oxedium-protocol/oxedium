96e9a50efa964c74f6fa8ba29fc8098a5b4fd42f
Arj-Lab <arj.lemited@gmail.com>
2025-12-11 00:04:09 +0300

    [ui] - animated connect button

--- app/lib/widgets/connect_wallet_button.dart
+++ app/lib/widgets/connect_wallet_button.dart
@@ -9,30 +9,123 @@ class ConnectWalletButton extends StatefulWidget {
   State<ConnectWalletButton> createState() => _ConnectWalletButtonState();
 }
 
-class _ConnectWalletButtonState extends State<ConnectWalletButton> {
-
+class _ConnectWalletButtonState extends State<ConnectWalletButton>
+    with SingleTickerProviderStateMixin {
   bool isHover = false;
+
+  late AnimationController _controller;
+  late Animation<double> _positionAnimation;
+  late Animation<double> _opacityAnimation;
+
+  final double buttonHeight = 35.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    _positionAnimation = Tween<double>(
+      begin: buttonHeight - 16, 
+      end: 0,          
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _opacityAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(begin: 0.0, end: 1.0).chain(CurveTween(curve: Curves.easeIn)),
+        weight: 40,
+      ),
+      TweenSequenceItem(
+        tween: Tween(begin: 1.0, end: 0.0).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 60,
+      ),
+    ]).animate(_controller);
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed && isHover) {
+        _controller.forward(from: 0);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    if (!_controller.isAnimating) {
+      _controller.forward(from: 0);
+    }
+  }
+
+  void _stopAnimation() {
+    _controller.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return CustomInkWell(
-      onTap: widget.onTap,
+      onTap: () {
+        _stopAnimation();
+        widget.onTap();
+      },
       onHover: (value) {
-        isHover = value;
-        setState(() {});
+        setState(() => isHover = value);
+        if (value) {
+          _startAnimation();
+        } else {
+          _stopAnimation();
+        }
       },
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 250),
-        height: 35.0,
-        width: 140.0,
-        alignment: Alignment.center,
+        height: buttonHeight,
+        width: 160.0,
+        padding: const EdgeInsets.symmetric(horizontal: 8),
         decoration: BoxDecoration(
-            color: isHover ? const Color(0xFF7637EC) : Colors.white,
-            borderRadius: BorderRadius.circular(500.0)),
-        child: AnimatedDefaultTextStyle(
-          duration: const Duration(milliseconds: 250),
-          style: TextStyle(color: isHover ? Colors.white : Colors.black, fontFamily: 'Aeonik', fontSize: 17.0),
-          child: const Text('Connect')),
+          color: isHover ? const Color(0xFF7637EC) : Colors.white,
+          borderRadius: BorderRadius.circular(500.0),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                if (!isHover) return const SizedBox();
+                return Positioned(
+                  left: 8,
+                  top: _positionAnimation.value,
+                  child: Opacity(
+                    opacity: _opacityAnimation.value,
+                    child: const Icon(
+                      Icons.power,
+                      size: 21.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              },
+            ),
+            AnimatedDefaultTextStyle(
+              duration: const Duration(milliseconds: 250),
+              style: TextStyle(
+                color: isHover ? Colors.white : Colors.black,
+                fontFamily: 'Aeonik',
+                fontSize: 17.0,
+              ),
+              child: const Text('Connect'),
+            ),
+          ],
+        ),
       ),
     );
   }
